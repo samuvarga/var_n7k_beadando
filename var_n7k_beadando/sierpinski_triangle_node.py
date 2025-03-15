d39d0606b7a9bbc1f7d92db0e031e4d398841e78
samuvarga <samuvarga24@gmail.com>
2025-03-15 12:11:44 +0100

    initial commit

--- var_n7k_beadando/sierpinski_triangle_node.py
+++ var_n7k_beadando/sierpinski_triangle_node.py
@@ -11,20 +11,20 @@ class SpiderWebNode(Node):
         self.timer = self.create_timer(0.2, self.loop)  # 0.2 másodpercenként
         self.cmd_pub = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
         self.loop_count = 0
-        self.radius = 50  # Kezdeti sugár a körökhöz
+        self.radius = 20  # Kezdeti sugár a körökhöz (kisebb)
         self.get_logger().info("Spider Web Node has been started")
 
     def loop(self):
         cmd_msg = Twist()
 
-        if self.loop_count < 12:  # 12 sugarat rajzolunk
+        if self.loop_count < 8:  # Kevesebb sugarat rajzolunk
             cmd_msg.linear.x = 1.0  # Előre mozgás
-            cmd_msg.angular.z = math.radians(30)  # 30 fokos elforgatás
-        elif self.loop_count < 24:  # 12 után rajzolunk egy kört
+            cmd_msg.angular.z = math.radians(45)  # 45 fokos elforgatás
+        elif self.loop_count < 16:  # 8 után rajzolunk egy kört
             cmd_msg.linear.x = 0.0  # Ne mozduljon előre
             cmd_msg.angular.z = 0.0  # Ne forgasson
             self.draw_circle(self.radius)  # Kör rajzolása
-            self.radius += 50  # Növeljük a sugár értékét a következő körhöz
+            self.radius += 20  # Növeljük a sugár értékét a következő körhöz
         else:
             cmd_msg.linear.x = 0.0  # Ne mozduljon előre
             cmd_msg.angular.z = 0.0  # Ne forgasson
@@ -34,8 +34,8 @@ class SpiderWebNode(Node):
         self.loop_count += 1
 
     def draw_circle(self, radius):
-        # Kör rajzolása kis lépésekben
-        for _ in range(36):  # 360 fok 10 fokos lépésekben
+        # Kör rajzolása kisebb lépésekben
+        for _ in range(18):  # 180 fokot rajzolunk 10 fokos lépésekben (kisebb kör)
             cmd_msg = Twist()
             cmd_msg.linear.x = 1.0  # Előre mozgás
             cmd_msg.angular.z = math.radians(10)  # 10 fokos elforgatás
