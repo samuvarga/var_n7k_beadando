6b22b7ebc7fdceca7788ef6f10d260a24ba3927f
samuvarga <samuvarga24@gmail.com>
2025-03-15 23:32:21 +0100

    foxglove2

--- launch/archery_target_launch.py
+++ launch/archery_target_launch.py
@@ -14,4 +14,19 @@ def generate_launch_description():
             executable='archery_target_node',
             output='screen'
         ),
+        Node(
+            package='foxglove_bridge',
+        executable='foxglove_bridge',
+        parameters=[
+            {'port': 8765},
+            {'address': '0.0.0.0'},
+            {'tls': False},
+            {'certfile': ''},
+            {'keyfile': ''},
+            #{'topic_whitelist': "'.*'"},
+            {'max_qos_depth': 10},
+            {'num_threads': 0},
+            {'use_sim_time': False},
+        ]
+        ),
     ])
