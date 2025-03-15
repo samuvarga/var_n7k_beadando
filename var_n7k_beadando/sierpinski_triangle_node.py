a3ef34fb2c3a79519d177f65b5bec3d08bd41ae7
samuvarga <samuvarga24@gmail.com>
2025-03-15 11:46:40 +0100

    initial commit

--- var_n7k_beadando/sierpinski_triangle_node.py
+++ var_n7k_beadando/sierpinski_triangle_node.py
@@ -1,36 +1,56 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
-import functools
 import time
 
-class CmdGen(Node):
+class SierpinskiTriangleNode(Node):
 
     def __init__(self):
-        super().__init__('cmd_gen')
-        self.timer = self.create_timer(0.2, self.loop)
-        self.cmd_pub = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
-        self.loop_count = 0
-        self.get_logger().info("cmd_gen_node [arj_intro_py] has been started")
+        super().__init__('sierpinski_triangle_node')
 
-    def loop(self):
+        # Publisher a cmd_vel topichoz, hogy mozgassuk a teknőst
+        self.cmd_pub = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
+
+        # Elindítjuk a timer-t, ami 0.5 másodpercenként hívja a move_turtle metódust
+        self.timer = self.create_timer(0.5, self.move_turtle)
+
+        # Különböző paraméterek a háromszög rajzolásához
+        self.side_length = 2.0  # A háromszög oldalának hossza
+        self.angle = 120.0      # A háromszög belső szögének értéke
+
+        # Inicializáljuk a számlálót, hogy nyomon követhessük, hány lépést tettünk
+        self.step_count = 0
+
+        self.get_logger().info("Sierpinski Triangle Node elindult")
+
+    def move_turtle(self):
+        # Üzenet a teknős mozgásához
         cmd_msg = Twist()
-        if self.loop_count < 20:
-            cmd_msg.linear.x = 1.0
-            cmd_msg.angular.z = 0.0
+
+        # Ha még nem rajzoltunk háromszöget, akkor kezdjük el
+        if self.step_count < 3:
+            cmd_msg.linear.x = self.side_length  # Haladás előre
+            cmd_msg.angular.z = 0.0  # Nincs elforgatás
+
+        # Ha már három oldal el van rajzolva, akkor forgatjuk a teknőst
         else:
-            cmd_msg.linear.x = -1.0
-            cmd_msg.angular.z = 1.0
+            cmd_msg.linear.x = 0.0  # Ne mozduljon előre
+            cmd_msg.angular.z = 1.0  # 1 rad/s sebességgel forgassuk
+
         self.cmd_pub.publish(cmd_msg)
-        self.loop_count += 1
-        if self.loop_count > 40:
-            self.loop_count = 0
+
+        # Frissítjük a lépéseinket
+        self.step_count += 1
+
+        # Ha elértük a három oldal rajzolását, akkor újra kezdjük
+        if self.step_count > 5:
+            self.step_count = 0
 
 def main(args=None):
     rclpy.init(args=args)
-    cmd_gen = CmdGen()
-    rclpy.spin(cmd_gen)
-    cmd_gen.destroy_node()
+    node = SierpinskiTriangleNode()
+    rclpy.spin(node)
+    node.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
