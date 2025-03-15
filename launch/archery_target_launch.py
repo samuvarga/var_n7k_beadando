061509fb1d695a8d9daeda868da185e839351969
samuvarga <samuvarga24@gmail.com>
2025-03-15 23:51:24 +0100

    initial commit

--- launch/archery_target_launch.py
+++ launch/archery_target_launch.py
@@ -15,18 +15,9 @@ def generate_launch_description():
             output='screen'
         ),
         Node(
-            package='foxglove_bridge',
-        executable='foxglove_bridge',
-        parameters=[
-            {'port': 8765},
-            {'address': '0.0.0.0'},
-            {'tls': False},
-            {'certfile': ''},
-            {'keyfile': ''},
-            #{'topic_whitelist': "'.*'"},
-            {'max_qos_depth': 10},
-            {'num_threads': 0},
-            {'use_sim_time': False},
-        ]
+            package='rqt_graph',
+            executable='rqt_graph',
+            name='rqt_graph',
+            output='screen'
         ),
     ])
