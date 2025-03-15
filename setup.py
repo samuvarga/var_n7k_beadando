3e1daf837843b7e76fd2e31cbb520fa9eae54010
samuvarga <samuvarga24@gmail.com>
2025-03-15 10:53:24 +0100

    initial commit

--- setup.py
+++ setup.py
@@ -1,29 +1,24 @@
-from setuptools import find_packages, setup
-from glob import glob
-import os
+from setuptools import setup
 
 package_name = 'var_n7k_beadando'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*')), 
+    packages=[package_name],
+    py_modules=[
+        'var_n7k_beadando.sierpinski_triangle_node'
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='todo',
-    maintainer_email='todo@todo.com',
+    maintainer='he',
+    maintainer_email='samuvarga24@gmail.com',
     description='TODO: Package description',
     license='GNU General Public License v3.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            # 'control_vehicle = var_n7k_beadando.control_vehicle:main',
+            'sierpinski_triangle_node = var_n7k_beadando.sierpinski_triangle_node:main'
         ],
     },
 )
