9c368cecb68aeb4a188e6b6e94cc133e315c63fd
samuvarga <samuvarga24@gmail.com>
2025-03-15 11:31:14 +0100

    initial commit

--- launch/sierpinski_triangle_launch.py
+++ launch/sierpinski_triangle_launch.py
@@ -11,7 +11,7 @@ def generate_launch_description():
         ),
         Node(
             package='var_n7k_beadando',
-            executable='sierpinski_triangle_node.py',
+            executable='sierpinski_triangle_node',
             output='screen'
         ),
     ])
