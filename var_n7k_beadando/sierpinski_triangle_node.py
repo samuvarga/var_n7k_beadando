04c98d9def1f2b1003581d9221756737a112467c
samuvarga <samuvarga24@gmail.com>
2025-03-15 11:57:03 +0100

    initial commit

--- var_n7k_beadando/sierpinski_triangle_node.py
+++ var_n7k_beadando/sierpinski_triangle_node.py
@@ -1,50 +1,88 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
-import time
+import math
 
 class SierpinskiTriangleNode(Node):
 
     def __init__(self):
         super().__init__('sierpinski_triangle_node')
 
-        # Publisher a cmd_vel topichoz, hogy mozgassuk a teknőst
+        # Publisher a cmd_vel topichoz
         self.cmd_pub = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
 
-        # Elindítjuk a timer-t, ami 0.5 másodpercenként hívja a move_turtle metódust
+        # Timer 0.5 másodperces intervallummal
         self.timer = self.create_timer(0.5, self.move_turtle)
 
-        # Különböző paraméterek a háromszög rajzolásához
-        self.side_length = 2.0  # A háromszög oldalának hossza
-        self.angle = 120.0      # A háromszög belső szögének értéke
+        # A háromszög oldalának hossza és az új háromszög kisebb oldalának hossza
+        self.side_length = 2.0  
+        self.angle = 120.0  # Háromszög belső szöge
 
-        # Inicializáljuk a számlálót, hogy nyomon követhessük, hány lépést tettünk
+        # Előrehaladás számláló
         self.step_count = 0
+
+        # Rekurzív háromszög rajzolás beállítása
+        self.depth = 4  # Mennyi szintű háromszöget rajzoljunk (rekurzív lépés)
 
         self.get_logger().info("Sierpinski Triangle Node elindult")
 
     def move_turtle(self):
-        # Üzenet a teknős mozgásához
         cmd_msg = Twist()
 
-        # Ha még nem rajzoltunk háromszöget, akkor kezdjük el
+        # Az első három oldal és forgatás
         if self.step_count < 3:
-            cmd_msg.linear.x = self.side_length  # Haladás előre
-            cmd_msg.angular.z = 0.0  # Nincs elforgatás
+            cmd_msg.linear.x = self.side_length  # Előrehaladás
+            cmd_msg.angular.z = 0.0  # Nincs forgatás
 
-        # Ha három oldal el van rajzolva, akkor forgatjuk a teknőst 120 fokkal
+        # Ha három oldal el van rajzolva, akkor forgatás 120 fokkal
         else:
-            cmd_msg.linear.x = 0.0  # Ne mozduljon előre
-            cmd_msg.angular.z = 1.57  # 120 fokos forgatás (rad)
+            cmd_msg.linear.x = 0.0  # Ne haladjon előre
+            cmd_msg.angular.z = math.radians(self.angle)  # 120 fokos forgatás
 
+        # Közzétesszük a parancsot
         self.cmd_pub.publish(cmd_msg)
 
-        # Frissítjük a lépéseinket
         self.step_count += 1
 
-        # Ha elértük a három oldal rajzolását, akkor újra kezdjük
+        # Ha elértük a három oldal rajzolását, újra kezdjük
         if self.step_count > 5:
             self.step_count = 0
+
+        # Rekurzív háromszög rajzolása (ha több szintet szeretnénk)
+        if self.step_count == 0 and self.depth > 0:
+            self.depth -= 1
+            self.draw_sierpinski(self.side_length)
+
+    def draw_sierpinski(self, side_length):
+        # Ha a háromszög elég kicsi, akkor ne rajzoljunk többet
+        if side_length < 0.1:
+            return
+        
+        # Rajzolj egy háromszöget
+        for _ in range(3):
+            self.move_forward(side_length)
+            self.turn_120_degrees()
+
+        # Rajzolj három kisebb háromszöget
+        for _ in range(3):
+            self.move_forward(side_length / 2)
+            self.turn_120_degrees()
+            self.draw_sierpinski(side_length / 2)
+            self.turn_120_degrees()
+
+    def move_forward(self, length):
+        # Előrehaladás
+        cmd_msg = Twist()
+        cmd_msg.linear.x = length
+        self.cmd_pub.publish(cmd_msg)
+        self.get_logger().info(f'Moving forward: {length}')
+
+    def turn_120_degrees(self):
+        # 120 fokos elforgatás
+        cmd_msg = Twist()
+        cmd_msg.angular.z = math.radians(120)  # 120 fok radianban
+        self.cmd_pub.publish(cmd_msg)
+        self.get_logger().info('Turning 120 degrees')
 
 def main(args=None):
     rclpy.init(args=args)
