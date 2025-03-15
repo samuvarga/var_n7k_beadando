42d98ab75ce65b0c3a2b95ae74087a2b2ffda0d9
samuvarga <samuvarga24@gmail.com>
2025-03-15 12:01:34 +0100

    initial commit

--- var_n7k_beadando/sierpinski_triangle_node.py
+++ var_n7k_beadando/sierpinski_triangle_node.py
@@ -3,92 +3,51 @@ from rclpy.node import Node
 from geometry_msgs.msg import Twist
 import math
 
-class SierpinskiTriangleNode(Node):
+class SpiderWebNode(Node):
 
     def __init__(self):
-        super().__init__('sierpinski_triangle_node')
-
-        # Publisher a cmd_vel topichoz
+        super().__init__('spider_web_node')
         self.cmd_pub = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
+        self.timer = self.create_timer(0.2, self.draw_spider_web)
+        self.angle = 0
+        self.radius = 50
+        self.loop_count = 0
 
-        # Timer 0.5 másodperces intervallummal
-        self.timer = self.create_timer(0.5, self.move_turtle)
-
-        # A háromszög oldalának hossza és az új háromszög kisebb oldalának hossza
-        self.side_length = 2.0  
-        self.angle = 120.0  # Háromszög belső szöge
-
-        # Előrehaladás számláló
-        self.step_count = 0
-
-        # Rekurzív háromszög rajzolás beállítása
-        self.depth = 4  # Mennyi szintű háromszöget rajzoljunk (rekurzív lépés)
-
-        self.get_logger().info("Sierpinski Triangle Node elindult")
-
-    def move_turtle(self):
+    def draw_spider_web(self):
+        # Létrehozzuk az üzenetet
         cmd_msg = Twist()
 
-        # Az első három oldal és forgatás
-        if self.step_count < 3:
-            cmd_msg.linear.x = self.side_length  # Előrehaladás
-            cmd_msg.angular.z = 0.0  # Nincs forgatás
-
-        # Ha három oldal el van rajzolva, akkor forgatás 120 fokkal
+        # Rajzoljuk a sugárvonalakat
+        if self.loop_count < 12:
+            cmd_msg.linear.x = 1.0  # Mozgás előre
+            cmd_msg.angular.z = math.radians(30)  # 30 fokos elforgatás
         else:
-            cmd_msg.linear.x = 0.0  # Ne haladjon előre
-            cmd_msg.angular.z = math.radians(self.angle)  # 120 fokos forgatás
+            # Rajzoljuk a koncentrikus köröket
+            cmd_msg.linear.x = 0.0  # Ne mozduljon előre
+            cmd_msg.angular.z = 0.0  # Nincs elforgatás
+            self.draw_circle(self.radius)  # Kör rajzolása
 
-        # Közzétesszük a parancsot
+        # Közzétesszük az üzenetet
         self.cmd_pub.publish(cmd_msg)
 
-        self.step_count += 1
+        self.loop_count += 1
+        if self.loop_count > 24:  # 12 vonal és 2 kör
+            self.loop_count = 0
+            self.radius += 50  # Következő körnél nagyobb sugár
 
-        # Ha elértük a három oldal rajzolását, újra kezdjük
-        if self.step_count > 5:
-            self.step_count = 0
-
-        # Rekurzív háromszög rajzolása (ha több szintet szeretnénk)
-        if self.step_count == 0 and self.depth > 0:
-            self.depth -= 1
-            self.draw_sierpinski(self.side_length)
-
-    def draw_sierpinski(self, side_length):
-        # Ha a háromszög elég kicsi, akkor ne rajzoljunk többet
-        if side_length < 0.1:
-            return
-        
-        # Rajzolj egy háromszöget
-        for _ in range(3):
-            self.move_forward(side_length)
-            self.turn_120_degrees()
-
-        # Rajzolj három kisebb háromszöget
-        for _ in range(3):
-            self.move_forward(side_length / 2)
-            self.turn_120_degrees()
-            self.draw_sierpinski(side_length / 2)
-            self.turn_120_degrees()
-
-    def move_forward(self, length):
-        # Előrehaladás
-        cmd_msg = Twist()
-        cmd_msg.linear.x = length
-        self.cmd_pub.publish(cmd_msg)
-        self.get_logger().info(f'Moving forward: {length}')
-
-    def turn_120_degrees(self):
-        # 120 fokos elforgatás
-        cmd_msg = Twist()
-        cmd_msg.angular.z = math.radians(120)  # 120 fok radianban
-        self.cmd_pub.publish(cmd_msg)
-        self.get_logger().info('Turning 120 degrees')
+    def draw_circle(self, radius):
+        # Kör rajzolása
+        for _ in range(36):  # Kicsi lépések a kör rajzolásához
+            cmd_msg = Twist()
+            cmd_msg.linear.x = 1.0
+            cmd_msg.angular.z = math.radians(10)  # Kis lépések a körhöz
+            self.cmd_pub.publish(cmd_msg)
 
 def main(args=None):
     rclpy.init(args=args)
-    node = SierpinskiTriangleNode()
-    rclpy.spin(node)
-    node.destroy_node()
+    spider_web_node = SpiderWebNode()
+    rclpy.spin(spider_web_node)
+    spider_web_node.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
